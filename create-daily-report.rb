c5514a452803a0c32df1cab21f619de0ddbbf699
Roland Tanglao <roland@rolandtanglao.com>
2023-12-07 11:04:38 -0800

    deleted some lines by mistake

--- create-daily-report.rb
+++ create-daily-report.rb
@@ -78,12 +78,13 @@ header_string = "|#{id_header}"
 TITLE_HEADER_LENGTH = 80
 TITLE_STR = 'Title'.freeze
 title_header = "#{TITLE_STR}#{NBSP_STR * ((TITLE_HEADER_LENGTH - TITLE_STR.length) + 1)}"
+header_string = "|#{title_header}"
 header_string += "|[O](## 'Operating System')|[T](## 'Topic')|[M](## 'Email Provider')|[A](## 'Antivirus')|[U](## 'User Chrome or other unsupported mod')|[Tags](## 'All Tags')|"
 output_markdown.push(header_string)
 EMOJI_HEADER_LENGTH = 1
 TAGS_HEADER_LENGTH = 40
 second_row_str =  "|#{DASH_STR * ID_HEADER_LENGTH}" # id
-second_row_str += "|#{DASH_STR * ID_HEADER_LENGTH}" # content
+second_row_str += "|#{DASH_STR * TITLE_HEADER_LENGTH}" # content
 second_row_str += "|#{DASH_STR * EMOJI_HEADER_LENGTH}" # OS
 second_row_str += "|#{DASH_STR * EMOJI_HEADER_LENGTH}" # TOPIC
 second_row_str += "|#{DASH_STR * EMOJI_HEADER_LENGTH}" # EMAIL PROVIDER
